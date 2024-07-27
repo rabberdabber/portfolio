4e1691c9a44c9edfcb473a4d8877e98f21c2af80
Bereket <bereketsiyum@gmail.com>
2024-07-27 18:30:15 +0900

    add avatar component and image in root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import React from "react";
-import { Boxes } from "@/components/ui/background-boxes";
 import { cn } from "@/lib/utils";
+import { Boxes } from "@/components/ui/background-boxes";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 
 export default function Home() {
@@ -10,6 +12,10 @@ export default function Home() {
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
       <Boxes />
+      <Avatar className="h-20 w-20">
+        <AvatarImage src="/avatar.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
       <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
         <span className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Bereket Assefa</span> is my name
       </h1>

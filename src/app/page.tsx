c7cfa26df2130e5624dbd5919d453c8293c9c7d6
Bereket <bereketsiyum@gmail.com>
2024-07-27 18:40:46 +0900

    add white border to avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4 flex flex-col items-center justify-center">
-        <Avatar className="h-20 w-20 absolute top-2 right-2">
+        <Avatar className="h-20 w-20 absolute top-2 right-2 border border-white">
          <AvatarImage src="/avatar.png" />
          <AvatarFallback>BA</AvatarFallback>
         </Avatar>

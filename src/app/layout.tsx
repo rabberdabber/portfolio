e421d4ec3128de8bc0fc93ab1b766f2d40c88fff
Bereket <bereketsiyum@gmail.com>
2025-01-22 23:21:20 +0900

    Add favicon.svg and update site configuration to use SVG icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: "/favicon.ico",
+    icon: "/favicon.svg",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
